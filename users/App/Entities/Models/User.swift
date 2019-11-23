9b3f3d87e1a666d354e80fd0b71aec61728af6f8
Vadim K <tongpo1965@yandex.ru>
2019-11-23 19:47:52 +0500

    Create models application

---
+++ users/App/Entities/Models/User.swift
@@ -0,0 +1,40 @@
+//
+//  User.swift
+//  users
+//
+//  Copyright © 2019 Вадим. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    let id: Int?
+    let firstName: String?
+    let lastName: String?
+    let email: String?
+    let avatarUrl: String?
+    var createdAt: Date? = nil
+
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case firstName = "first_name"
+        case lastName = "last_name"
+        case email = "email"
+        case avatarUrl = "avatar_url"
+        case createdAt = "created_at"
+    }
+
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        id = try values.decodeIfPresent(Int.self, forKey: .id)
+        firstName = try values.decodeIfPresent(String.self, forKey: .firstName)
+        lastName = try values.decodeIfPresent(String.self, forKey: .lastName)
+        email = try values.decodeIfPresent(String.self, forKey: .email)
+        avatarUrl = try values.decodeIfPresent(String.self, forKey: .avatarUrl)
+        if let createdAt = try values.decodeIfPresent(String.self, forKey: .createdAt) {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ"
+            self.createdAt = formatter.date(from: createdAt)
+        }
+    }
+}
